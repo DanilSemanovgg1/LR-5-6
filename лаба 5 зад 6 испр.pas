6999305494805ed591945e83b6aac220cf36f818
DanilSemanovgg1 <144227090+DanilSemanovgg1@users.noreply.github.com>
2023-10-31 10:06:06 +0300

    Add files via upload

---
+++ лаба 5 зад 6 испр.pas
@@ -0,0 +1,20 @@
+﻿program zad6;
+var
+  n: array [1..5] of integer;
+  i, f: integer;
+
+begin
+  write('Введите значение массива ');
+  for i := 1 to 5 do
+  begin
+    read(n[i]);
+  end;
+  begin
+    for i := 2 to 5 do
+      if n[i] > n[i - 1] then f := 1;
+    if f = 1 then 
+      writeln('Массив упорядочен') 
+      else writeln('Массив не упорядочен');
+  end;
+  write(n);
+end.
